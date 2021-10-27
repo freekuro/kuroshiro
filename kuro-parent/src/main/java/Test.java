bf1227cc0f0c7a019367750da46d5f6fb8ce7b8d
zhangdirong <freekuro@126.com>
2021-10-27 09:05:50 +0800

    2

---
+++ kuro-parent/src/main/java/Test.java
@@ -0,0 +1,14 @@
+/**
+* <p>
+* </p>
+*
+* @author zhangdirong
+* @date 2021/10/27 9:05 上午
+*/
+public class Test {
+
+    public static void main(String[] args) {
+        String s = "s";
+    }
+
+}
